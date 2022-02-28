a9ce2464553ef7767efbbfd43e186ff94c587a71
0x_Anakin <praditsas@open.gr>
2022-02-28 16:26:22 +0200

    fetch example

--- modules/my-react-app/src/App.js
+++ modules/my-react-app/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { decode as base64_decode, encode as base64_encode } from "base-64";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const WC_KEY = 42150;
+  const SITE_ID = 20123;
+  const headers = new Headers();
+
+  headers.set("Authorization", `Basic ${base64_encode("test@liferay.com:test")}`);
+
+  useEffect(() => {
+
+    fetch(`http://localhost:8080/o/headless-delivery/v1.0/sites/${SITE_ID}/structured-contents/by-key/${WC_KEY}`, {
+      method: "GET",
+      headers: headers
+    }).then(res => res.json()).then((result) => {
+      setData(result);
+    }).catch((err) => {
+      setError(err);
+    })
+
+  }, [])
+
+  if (!error && !data) {
+    return (
+      <div>Loading...</div>
+    )
+  } else if (error) {
+    return (
+      <div>Error: {error.message}</div>
+    )
+  } else {
+    return (
+      <div className="custom-user-container">
+        <h1 class="user-full-name">
+          <span class="user-first-name">{data.contentFields[0].contentFieldValue.data}</span>&nbsp;
+          <span class="user-last-name">{data.contentFields[1].contentFieldValue.data}</span>
+        </h1>
+        <img src={data.contentFields[2].contentFieldValue.image.contentUrl} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- modules/my-react-widget/src/AppComponent.js
+++ modules/my-react-widget/src/AppComponent.js
@@ -5,11 +5,9 @@ export default class extends React.Component {
 		return (
             <div>
 
-				<h1>No localization</h1>
+				<h1>{Liferay.Language.get('react-welcome-message')}</h1>
 
-				{/* <h1>{Liferay.Language.get('react-welcome-message')}</h1> */}
-
-				{/* <div>
+				<div>
         	        <span className="tag">{Liferay.Language.get('portlet-namespace')}:</span> 
 					<span className="value">{this.props.portletNamespace}</span>
 				</div>
@@ -25,7 +23,7 @@ export default class extends React.Component {
 				<div>
 					<span className="tag">{Liferay.Language.get('configuration')}:</span>
 					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
-				</div> */}
+				</div>
 				
 			</div>
 		);
